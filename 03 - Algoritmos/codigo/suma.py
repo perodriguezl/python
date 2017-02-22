2e6a8490b47966cd3896ed935f609c5dbd425e0f
Pedro <perodriguezl@gmail.com>
2017-02-22 19:50:40 -0400

    first commit

---
+++ 03 - Algoritmos/codigo/suma.py
@@ -0,0 +1,6 @@
+valor1 = input("Primer Valor: ") #entrada de datos
+valor2 = input("Segundo Valor: ")#entrada de datos 
+
+suma = valor1 + valor2 #procesos aritmetico 
+
+print("Resultado:" + suma) #salida de informacion
---
+++ 03 - Algoritmos/codigo/sumaMejorada.py
@@ -0,0 +1,6 @@
+valor1 = input("Primer Valor Entero: ") #entrada de datos
+valor2 = input("Segundo Valor Entero: ")#entrada de datos 
+
+suma = int(valor1) + int(valor2) #procesos aritmetico 
+
+print("Resultado:" + str(suma)) #salida de informacion
